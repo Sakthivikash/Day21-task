1763b230b3bfa85e9d411a5338e68e6cec9d55d2
Sakthivikash <93461941+Sakthivikash@users.noreply.github.com>
2022-03-03 14:11:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+
+setTimeout(()=> {
+    const content= document.getElementById("main");
+    content.innerHTML="10";
+    
+    setTimeout(()=>{
+        const content= document.getElementById("main");
+      content.innerHTML="9";
+      
+       setTimeout(()=>{
+        const content= document.getElementById("main");
+        content.innerHTML="8";
+        
+          setTimeout(()=>{
+            const content= document.getElementById("main");
+            content.innerHTML="7";
+              
+              setTimeout(()=>{
+                const content= document.getElementById("main");
+                content.innerHTML="6";
+                
+                  setTimeout(()=>{
+                    const content= document.getElementById("main");
+                    content.innerHTML="5";
+                   
+                    setTimeout(()=>{
+                        const content= document.getElementById("main");
+                      content.innerHTML="4";
+                
+                      setTimeout(()=>{
+                        const content= document.getElementById("main");
+                        content.innerHTML="3";
+                          
+                          setTimeout(()=>{
+                            const content= document.getElementById("main");
+                            content.innerHTML="2";
+                            
+                              setTimeout(()=>{
+                                const content= document.getElementById("main");
+                                content.innerHTML="1";
+                                
+                                  setTimeout(()=>{
+                                    const box=document.getElementById('content').style.borderRadius="10px";
+            
+                                    const content= document.getElementById("main");
+                                    content.style.fontSize="80px";
+                                    content.innerHTML="🎇🎆Happy Independence Day️✨🎏";
+
+                                    const message= document.getElementById('msg').innerHTML="Start the celebration now🥳";
+                                    
+                                  }, 1000)
+                              },1000)
+                          },1000)
+                      },1000)
+                    },1000)
+                },1000)
+              },1000)
+          }, 1000)
+      }, 1000)
+    }, 1000)
+}, 1000);
